2a42f778cd9c81cf36d137a9dda6bdc95f24537f
crusstyy <35669933+crusstyy@users.noreply.github.com>
2018-11-12 15:36:34 -0800

    Update options.module.lua

--- roblox/trello.proxy.module/options.module.lua
+++ roblox/trello.proxy.module/options.module.lua
@@ -1,6 +1,6 @@
 return {
   BaseUrl = 'https://my-app.herokuapp.com',
-  Token = 'my-access-token',
+  Token = 'e16e4832bd670badefaf1a5f3c8df00f6768ca449287c67c6ec21bf927e14bb8',
   Headers = {
     Method = 'X-HTTP-Method-Override',
 	Token = 'X-Auth-Token'
